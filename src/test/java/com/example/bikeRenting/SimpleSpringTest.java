66e3b693b085c32ea6820466905daf4895a32855
WojcikMikolaj <52234302+WojcikMikolaj@users.noreply.github.com>
2021-04-08 11:52:28 +0200

    Added TestConfiguration from https://stackoverflow.com/questions/50915868/spring-boot-and-flyway-clear-database-data-before-integration-tests.

--- src/test/java/com/example/bikeRenting/SimpleSpringTest.java
+++ src/test/java/com/example/bikeRenting/SimpleSpringTest.java
@@ -1,13 +1,25 @@
 package com.example.bikeRenting;
 
+import configuration.FlywayMigrationConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.event.annotation.PrepareTestInstance;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("tests")
+@Import(FlywayMigrationConfig.class)
+@Transactional
 class SimpleSpringTest {
+
+
+    @Autowired
+    private FlywayMigrationStrategy strategy;
 
     @PrepareTestInstance
     void prepareInstance() {
