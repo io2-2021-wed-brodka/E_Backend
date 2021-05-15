25ced5d61b801b0d58c74b7d67e771ba6c4b4dc5
Wiktor Rzemek <wrzemek@ittouch.com>
2021-05-15 21:27:46 +0200

    fixed getRentals

--- src/main/java/com/example/bikeRenting/repository/RentalRepository.java
+++ src/main/java/com/example/bikeRenting/repository/RentalRepository.java
@@ -10,6 +10,7 @@ import javax.websocket.server.PathParam;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.Set;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     @Query("select r from Rental r " +
@@ -17,4 +18,11 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
             "where b.id = :bikeId " +
             "and r.endDate is null")
     Optional<Rental> getCurrentBikeRental(@PathParam("bikeId") long bikeId);
+
+    @Query("select r from Rental r " +
+            "join r.user u " +
+            "join r.bike b " +
+            "where u.userName = :username " +
+            "and r.toStation is null")
+    Set<Rental> getCurrentUserRentals(@PathParam("username") String username);
 }
