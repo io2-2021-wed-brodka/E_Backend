f8a4d459f3604d2cea7b9f048b1f7ab35c0a4381
Przemyslaw Wcislo <pwcislo@ittouch.com>
2021-06-01 22:19:10 +0200

    add malfunction api endpoints

---
+++ src/main/java/com/example/bikeRenting/api/MalfunctionController.java
@@ -0,0 +1,43 @@
+package com.example.bikeRenting.api;
+
+import com.example.bikeRenting.constants.RoleConstants;
+import com.example.bikeRenting.dto.request.malfunction.AddMalfunctionRequestDTO;
+import com.example.bikeRenting.dto.response.MalfunctionDTO;
+import com.example.bikeRenting.dto.response.MalfunctionListDTO;
+import com.example.bikeRenting.service.malfunction.MalfunctionService;
+import io.swagger.annotations.Api;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/malfunctions")
+@Api(description = "API for managing bike malfunctions")
+public class MalfunctionController {
+
+    private final MalfunctionService malfunctionService;
+
+    public MalfunctionController(MalfunctionService malfunctionService) {
+        this.malfunctionService = malfunctionService;
+    }
+
+    @GetMapping("/")
+    @Secured(RoleConstants.TECH)
+    public MalfunctionListDTO getAllMalfunctions() {
+        return malfunctionService.getAllMalfunctions();
+    }
+
+    @PostMapping("/")
+    @Secured(RoleConstants.TECH)
+    public MalfunctionDTO addNewMalfunction(@RequestBody AddMalfunctionRequestDTO addMalfunctionRequestDTO, Principal principal) {
+        return malfunctionService.addNewMalfunction(addMalfunctionRequestDTO, principal.getName());
+    }
+
+    @DeleteMapping("/{malfunctionId}")
+    @Secured(RoleConstants.TECH)
+    public void deleteMalfunction(@PathVariable Long malfunctionId) {
+        malfunctionService.deleteMalfunction(malfunctionId);
+    }
+
+}
