fee60427cdd4cd9aa729bb7d1f8a9bcdd82c8e8e
Jakub Stefaniuk <jstefaniuk@ittouch.com>
2021-06-15 22:27:55 +0200

    tests update

--- src/main/java/com/example/bikeRenting/dto/request/bikeStation/BlockStationRequestDTO.java
+++ src/main/java/com/example/bikeRenting/dto/request/bikeStation/BlockStationRequestDTO.java
@@ -1,8 +1,10 @@
 package com.example.bikeRenting.dto.request.bikeStation;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BlockStationRequestDTO {
 
     private Long id;
